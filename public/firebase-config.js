0ca69e05943f49464ee71b7031e3370546519139
Rafael Cerri <rafaelbcerri@gmail.com>
2018-08-06 16:19:35 -0300

    All

---
+++ public/firebase-config.js
@@ -0,0 +1,10 @@
+var config = {
+  apiKey: "AIzaSyB99CkaRWWMHhUkiz_voUsAnDDzS9mICLQ",
+  authDomain: "banana-peixinho.firebaseapp.com",
+  databaseURL: "https://banana-peixinho.firebaseio.com",
+  projectId: "banana-peixinho",
+  storageBucket: "banana-peixinho.appspot.com",
+  messagingSenderId: "1021082723014"
+};
+
+firebase.initializeApp(config);
